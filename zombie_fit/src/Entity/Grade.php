e4461f43e570240597f3e0edeee57b9a5b364076
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2023-09-19 12:26:27 +0200

    first commit

---
+++ zombie_fit/src/Entity/Grade.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GradeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GradeRepository::class)]
+class Grade
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
